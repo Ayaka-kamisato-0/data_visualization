cf7cd0753924edc0ae24010926bec6e59e68308e
boilboil89 <3220101840@zju.edu,cn>
2024-07-23 15:30:20 +0800

    0723

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,9 @@
+//黄沁心，我并没有在增加这段话后的commit信息中标明我增加了段注释
+//Are you really able to see this?
+//I am just testing whether you can see this or not.
+//If you did not see this, its really obvious that its necessary for commit messages to be clear and concise.
+//I'll tell you about this very soon if you did not see this. 
+//So if you are looking at this, STOP USING FUCKING DATE FOR COMMIT MESSAGES!!!!!!!
 
 // 不同请求的处理
 const reducer = (state, action) => {
